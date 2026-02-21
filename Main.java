bbf1249a8f824e4356955c83ea8d730f33284f53
Gustavo Heiden <gustavo.heiden32@gmail.com>
2026-02-20 22:38:32 -0300

    vogaisExer-1

--- Main.java
+++ Main.java
@@ -1,3 +1,21 @@
 public static void main() {
 
-}
+    Scanner sc = new Scanner(System.in);
+    System.out.println("escreva uma palavra");
+    String palavra = sc.nextLine();
+
+    String vogais = "aeiou";
+    int contador = 0;
+    String vogaisCon = "";
+    for(int i=0;i<palavra.length();i++) {
+        char letra = palavra.charAt(i);
+        if(vogais.indexOf(letra) != -1){
+            contador++;
+            vogaisCon = vogaisCon + letra + " ";
+        }
+        }
+    System.out.println("na sua palavra tem "+contador+" vogais");
+    System.out.println("e elas sao:" + vogaisCon);
+
+    }
+
